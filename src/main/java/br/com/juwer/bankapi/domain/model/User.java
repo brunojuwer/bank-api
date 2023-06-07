3214eac45b70db0ec4ffe2f723725a44f1927f5a
Bruno Juwer <bruno.juwer1997@gmail.com>
2023-06-07 14:33:56 -0300

    Created application

---
+++ src/main/java/br/com/juwer/bankapi/domain/model/User.java
@@ -0,0 +1,25 @@
+package br.com.juwer.bankapi.domain.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "_user")
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+}
