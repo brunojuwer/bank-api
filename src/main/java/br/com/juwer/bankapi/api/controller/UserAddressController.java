8aaced0963e4908de6c3d8c46c71a5842b35d719
Bruno Juwer <bruno.juwer1997@gmail.com>
2023-06-09 17:59:37 -0300

    Added address controller

---
+++ src/main/java/br/com/juwer/bankapi/api/controller/UserAddressController.java
@@ -0,0 +1,13 @@
+package br.com.juwer.bankapi.api.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/users/{userId}/address")
+@RequiredArgsConstructor
+public class UserAddressController {
+
+
+}
